36d3aa3d5bbdf9732cc41a300de67c68a85566bb
eigenlambda123 <rmvilla987@gmail.com>
2025-08-03 14:31:27 +0800

    Implemented CLI interaction menu

--- 01_personal_contact_manager/src/Main.java
+++ 01_personal_contact_manager/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Contact Manager");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nPersonal Contact Manager:");
+            System.out.println("[1] Add Contact");
+            System.out.println("[2] View All Contacts");
+            System.out.println("[3] Exit");
+            System.out.print("Select an option: ");
+            
+            // Input Placeholder
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+                    System.out.println("Add Contact selected.");
+                    // TODO: Add contact logic
+                    break;
+                case "2":
+                    System.out.println("View All Contacts selected.");
+                    // TODO: View contact logic
+                    break;
+                case "3":
+                    System.out.println("Exiting Contact Manager...");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        scanner.close();
     }
 }
